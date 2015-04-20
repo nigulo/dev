9a707888699319461b77c9e4affe7fc843201d82
nigulo <nigulolspert@hotmail.com>
2015-04-20 10:56:27 +0300

    Changes in HH

--- Cpp/HilbertHuang/src/HilbertHuang.cpp
+++ Cpp/HilbertHuang/src/HilbertHuang.cpp
@@ -3,35 +3,42 @@
 #include <fftw3.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string>
+#include <vector>
+//#include <iostream>
+#include <fstream>
+//#include <cstdlib>
+//#include <sstream>
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
 
-int main(int argc, char** argv) {
-	return 0;
+string fileName;
+string prefix;
+double precision = 0.01;
+bool header = true;
+double xScale = 1;
+
+void multComplex(double x[2], double y[2]) {
+	double x0 = x[0];
+	x[0] = x0 * y[0] - x[1] * y[1];
+	x[1] = x[1] * y[0] + x0 * y[1];
 }
 
-void analyticSignal(int n, double* realSignal) {
-    fftw_complex* conjugatedSignal = (fftw_complex*) malloc(sizeof(fftw_complex) * n);
-    for (int i = 0; i < n; i++) {
-    	conjugatedSignal[0] = realSignal[i];
-    	conjugatedSignal[1] = 0;
-    }
-	fft(true, n, conjugatedSignal, 0);
-	fft(false, n, conjugatedSignal, (M_PI_2));
-	double* amplitude = (double*) malloc(sizeof(double) * n);
-	double* phase = (double*) malloc(sizeof(double) * n);
-	double* frequency = (double*) malloc(sizeof(double) * (n - 2));
-	for (int i = 0; i < n; i++) {
-		double u = realSignal[i];
-		double v = conjugatedSignal[i][0];
-		double u2v2 = u * u + v * v;
-		amplitude[i] = sqrt(u2v2);
-		phase[i] = atan(v / u);
-
-		if (i > 0 && i < n - 1) {
-			frequency[i] = (getRealTangent(i, conjugatedSignal) * u - getTangent(i, realSignal) * v) / u2v2;
+void shift(int n, fftw_complex* data, double phaseShift) {
+	int nDiv2 = n >> 1;
+	double c1[2] = {cos(-phaseShift), sin(-phaseShift)};
+	double c2[2] = {cos(phaseShift), sin(phaseShift)};
+	if (phaseShift != 0) {
+		for (int i = 1; i < n; i++) {
+			if (i != nDiv2) {
+				if (i < nDiv2) {
+					multComplex(data[i], c1);
+				} else if (i > nDiv2) {
+					multComplex(data[i], c2);
+				}
+			}
 		}
-
 	}
 }
 
@@ -52,33 +59,72 @@ void fft(bool direction, int n, fftw_complex* data, double phaseShift) {
 
 }
 
-void shift(int n, fftw_complex* data, double phaseShift) {
-	int nDiv2 = n >> 1;
-	double c1[2] = {cos(-phaseShift), sin(-phaseShift)};
-	double c2[2] = {cos(phaseShift), sin(phaseShift)};
-	if (phaseShift != 0) {
-		for (int i = 1; i < n; i++) {
-			if (i != nDiv2) {
-				if (i < nDiv2) {
-					multComplex(data[i], c1);
-				} else if (i > nDiv2) {
-					multComplex(data[i], c2);
-				}
-			}
-		}
-	}
-}
-
-void multComplex(double x[], double y[]) {
-	double x0 = x[0];
-	x[0] = x0 * y[0] - x[1] * y[1];
-	x[1] = x[1] * y[0] + x0 * y[1];
-}
-
 double getTangent(int i, double* data) {
 	return (data[i + 1] - data[i - 1]) / 2;
 }
 
 double getRealTangent(int i, fftw_complex* data) {
 	return (data[i + 1][0] - data[i - 1][0]) / 2;
+}
+
+void analyticSignal(int n, double* realSignal) {
+    fftw_complex* conjugatedSignal = (fftw_complex*) malloc(sizeof(fftw_complex) * n);
+    for (int i = 0; i < n; i++) {
+    	conjugatedSignal[i][0] = realSignal[i];
+    	conjugatedSignal[i][1] = 0;
+    }
+	fft(true, n, conjugatedSignal, 0);
+	fft(false, n, conjugatedSignal, (M_PI_2));
+	double* amplitude = (double*) malloc(sizeof(double) * n);
+	double* phase = (double*) malloc(sizeof(double) * n);
+	double* frequency = (double*) malloc(sizeof(double) * (n - 2));
+	for (int i = 0; i < n; i++) {
+		double u = realSignal[i];
+		double v = conjugatedSignal[i][0];
+		double u2v2 = u * u + v * v;
+		amplitude[i] = sqrt(u2v2);
+		phase[i] = atan(v / u);
+		if (i > 0 && i < n - 1) {
+			frequency[i] = (getRealTangent(i, conjugatedSignal) * u - getTangent(i, realSignal) * v) / u2v2;
+		}
+	}
+}
+
+int main(int argc, char** argv) {
+	fileName = argv[0];
+	string::size_type n = fileName.find('.');
+	prefix = fileName.substr(0, n);
+
+	ifstream input(fileName);
+	vector<double> xs, ys;
+	for (string line; getline(input, line);) {
+		//cout << line << endl;
+		std::vector<std::string> words;
+		boost::split(words, line, boost::is_any_of("\t "), boost::token_compress_on);
+		for (vector<string>::iterator it = words.begin() ; it != words.end(); ++it) {
+			if ((*it).length() == 0) {
+				words.erase(it);
+			}
+		}
+		if (words.size() > 0 && words[0][0] == '#') {
+			cout << "Skipping comment line: " << line << endl;
+		} else if (words.size() == 2) {
+			try {
+				double xVal = stod(words[0]);
+				double yVal = stod(words[1]);
+				xs.push_back(xVal);
+				ys.push_back(yVal);
+			} catch (invalid_argument& ex) {
+				cout << "Skipping line, invalid number: " << line << endl;
+			}
+		} else {
+			cout << "Skipping line, invalid number of columns: " << line << endl;
+		}
+    }
+	input.close();
+	if (xs.size() > 1) {
+		xScale = xs[1] - xs[0]; // Assuming even sampling
+	}
+
+	return 0;
 }
