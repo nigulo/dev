ac009b081e95ae94ff643d3fb54bb1d2d4f3bcb4
nigulo <nigulolspert@hotmail.com>
2015-05-20 14:58:49 +0300

    Butterfly wing width calculation

--- Cpp/butterfly/main.cpp
+++ Cpp/butterfly/main.cpp
@@ -92,7 +92,104 @@ void collect(double depth, const string& fileNamePattern, unsigned sampling) {
 	output.close();
 }
 
+double getLat(const string& fileName) {
+	int index = fileName.find('_');
+	index = fileName.find('_', index + 1);
+	string latStr = fileName.substr(index + 1);
+	double lat = -1;
+	for (unsigned i = 0; i < latStr.length(); i++) {
+		if (!isdigit(latStr[i])) {
+			lat = stod(latStr.substr(0, i));
+			break;
+		}
+	}
+	return lat;
+}
+
+double minLat = 19;
+double maxLat = 31;
+double threshold = 4;
+
+void wings(bool nOrS) {
+	vector<double> ts;
+	vector<tuple<double /*minLat*/, double /*maxLat*/>> wings;
+	directory_iterator end_itr; // default construction yields past-the-end
+	path currentDir(".");
+	for (directory_iterator itr(currentDir); itr != end_itr; ++itr) {
+		if (is_regular_file(itr->status())) {
+			const string& fileName = itr->path().generic_string();
+			if (fileName.substr(fileName.length() - 4) != ".txt") {
+				continue;
+			}
+		    cout << "Processing " << fileName << endl;
+			double lat = getLat(fileName);
+			if (nOrS && lat < 127.5) {
+				continue;
+			}
+			if (lat < 15 || lat > 240) {
+				continue;
+			}
+			lat = (lat - 127.5) * 150 / 255;
+			if (abs(lat) < minLat || abs(lat) > maxLat) {
+				continue;
+			}
+			ifstream input(fileName);
+			unsigned i = 0;
+			for (string line; getline(input, line);) {
+				//cout << line << endl;
+				std::vector<std::string> words;
+				boost::split(words, line, boost::is_any_of("\t "), boost::token_compress_on);
+				for (vector<string>::iterator it = words.begin(); it != words.end();) {
+					//cout << "<" << (*it) << ">" << endl;
+					if ((*it).length() == 0) {
+						it = words.erase(it);
+					} else {
+						it++;
+					}
+				}
+				if (words.size() > 0 && words[0][0] == '#') {
+					//cout << "Skipping comment line: " << line << endl;
+				} else if (words.size() == 2) {
+					try {
+						double t = stod(words[0]);
+						double b = stod(words[1]);
+						if (wings.size() <= i) {
+							ts.push_back(t);
+							wings.push_back(make_tuple(90, 0));
+						}
+						auto wing = wings[i];
+						if (abs(b) >= threshold) {
+							if (abs(lat) < abs(get<0>(wing))) {
+								wings[i] = make_tuple(lat, get<1>(wing));
+							} else if (abs(lat) > abs(get<1>(wing))) {
+								wings[i] = make_tuple(get<0>(wing), lat);
+							}
+						}
+						i++;
+					} catch (invalid_argument& ex) {
+						cout << "Skipping line, invalid number: " << line << endl;
+					}
+				} else {
+					cout << "Skipping line, invalid number of columns: " << line << endl;
+				}
+		    }
+			input.close();
+		}
+	}
+	ofstream output(string("wings.csv"));
+	for (unsigned i = 0; i < ts.size(); i++) {
+		if (get<0>(wings[i]) < 90 && get<1>(wings[i]) > 0) {
+			output << ts[i] << " " << get<0>(wings[i]) << " " << get<1>(wings[i]) << " " << (get<1>(wings[i]) - get<0>(wings[i])) << endl;
+		}
+	}
+	output.close();
+}
+
 int main(int argc, char** argv) {
+	if (argc == 2) {
+		wings(string(argv[1]) == "N" || string(argv[1]) == "n");
+		return EXIT_SUCCESS;
+	}
 	if (argc >= 3) {
 		unsigned sampling = 100;
 		if (argc >= 4) {
